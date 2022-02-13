288b2b60a038de44850b2ac7dc6807ce40906fa0
Harinandan <harinandan.alukkal@gmail.com>
2022-02-14 00:40:03 +0530

    Added popup

--- lib/views/message/receive_message.dart
+++ lib/views/message/receive_message.dart
@@ -1,4 +1,6 @@
+import 'package:anuragam_website/controllers/submission.dart';
 import 'package:anuragam_website/views/components/receive_field.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class ReceiveMessage extends StatelessWidget {
@@ -8,13 +10,43 @@ class ReceiveMessage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ReceiveTemplate(
       title: 'Receive Message',
-      text1: 'Feeling a bit lonely on Valentine\'s? Don\'t worry, there\'s no scarcity of love today',
-      text2: 'Click on the receive button to receive a message written by a stranger and who knows, maybe a dose of unfiltered and unconditional love is what you might need',
+      text1:
+          'Feeling a bit lonely on Valentine\'s? Don\'t worry, there\'s no scarcity of love today',
+      text2:
+          'Click on the receive button to receive a message written by a stranger and who knows, maybe a dose of unfiltered and unconditional love is what you might need',
       imagePath: 'assets/receive_message.png',
-      onPressed: () {
-        print('Message Received');
+      onPressed: () async {
+        final submission = await SubmissionInterface.getSubmission();
+        print(submission);
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: 
+                // height: 400,
+                // child: Column(
+                  // mainAxisSize: MainAxisSize.min,
+                  // children: [
+                    Text(
+                      submission.message,
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                //     AutoSizeText(
+                //       '-' + submission.contact,
+                //       style: TextStyle(
+                //         fontSize: 18,
+                //       ),
+                //       textAlign: TextAlign.right,
+                //     )
+                //   ],
+                // ),
+              // ),
+            );
+          },
+        );
       },
     );
   }
 }
-
